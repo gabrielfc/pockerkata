6fc2329031e94f3739e40bf31b21e7275f40d328
Gabriel Campolina <gcampolina@fitec.org.br>
2018-10-15 20:56:39 -0300

    Separação dos métodos

--- pockerkata/src/main/java/br/org/scrumclass/pockerkata/Pocker.java
+++ pockerkata/src/main/java/br/org/scrumclass/pockerkata/Pocker.java
@@ -2,8 +2,40 @@ package br.org.scrumclass.pockerkata;
 
 public class Pocker {
 
-	public static Boolean play(String[] jogador1, String[] jogador2) {
-		return true;
+	public static int play(String[] jogador1, String[] jogador2) {
+	    	int resultado = 0;
+	    	resultado = validaRegraTrinca(jogador1, jogador2);
+	    	if(resultado != 0){
+	    	    return resultado;
+	    	}
+	    	resultado = validaRegraDoisPar(jogador1, jogador2);
+	    	if(resultado != 0){
+	    	    return resultado;
+	    	}
+	    	resultado = validaRegraUmPar(jogador1, jogador2);
+	    	if(resultado != 0){
+	    	    return resultado;
+	    	}
+	    	return validaRegraMaiorCarta(jogador1, jogador2);
 	}
-	
+
+	private static int validaRegraMaiorCarta(String[] jogador1, String[] jogador2) {
+	    // TODO Auto-generated method stub
+	    return 0;
+	}
+
+	private static int validaRegraUmPar(String[] jogador1, String[] jogador2) {
+	    // TODO Auto-generated method stub
+	    return 0;
+	}
+
+	private static int validaRegraDoisPar(String[] jogador1, String[] jogador2) {
+	    // TODO Auto-generated method stub
+	    return 0;
+	}
+
+	private static int validaRegraTrinca(String[] jogador1, String[] jogador2) {
+	    // TODO Auto-generated method stub
+	    return 0;
+	}
 }
--- pockerkata/src/test/java/br/org/scrumclass/pockerkata/AppTest.java
+++ pockerkata/src/test/java/br/org/scrumclass/pockerkata/AppTest.java
@@ -13,34 +13,34 @@ public class AppTest extends TestCase
     public void testeComAs() {
     	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
     	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(Pocker.play(jogador1, jogador2));
+    	assertTrue(Pocker.play(jogador1, jogador2) == 1);
     }
     
     @Test
     public void testeComPar() {
     	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
     	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(!Pocker.play(jogador1, jogador2));
+    	assertTrue(Pocker.play(jogador1, jogador2) == 1);
     }
     
     @Test
     public void testeComDoisPares() {
     	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
     	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(!Pocker.play(jogador1, jogador2));
+    	assertTrue(Pocker.play(jogador1, jogador2) == 1);
     }
     
     @Test
     public void testeComTrica() {
     	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
     	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(!Pocker.play(jogador1, jogador2));
+    	assertTrue(Pocker.play(jogador1, jogador2) == 1);
     }
     
     @Test
     public void testeComSequencia() {
     	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
     	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(!Pocker.play(jogador1, jogador2));
+    	assertTrue(Pocker.play(jogador1, jogador2) == 1);
     }
 }
