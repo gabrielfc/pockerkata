be7af8e636a85c24831528353085ad6d0ca9529b
Eduardo de Figueiredo Júnior <efigueiredojunior@gmail.com>
2018-10-15 21:49:07 -0300

    comentando metodos nao usados

--- pockerkata/src/test/java/br/org/scrumclass/pockerkata/AppTest.java
+++ pockerkata/src/test/java/br/org/scrumclass/pockerkata/AppTest.java
@@ -16,31 +16,31 @@ public class AppTest extends TestCase
     	assertTrue(Pocker.play(jogador1, jogador2) == 2);
     }
     
-    @Test
-    public void testeComPar() {
-    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
-    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(false);
-    }
-    
-    @Test
-    public void testeComDoisPares() {
-    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
-    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(false);
-    }
-    
-    @Test
-    public void testeComTrica() {
-    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
-    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(false);
-    }
-    
-    @Test
-    public void testeComSequencia() {
-    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
-    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
-    	assertTrue(false);
-    }
+//    @Test
+//    public void testeComPar() {
+//    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
+//    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
+//    	assertTrue(false);
+//    }
+//    
+//    @Test
+//    public void testeComDoisPares() {
+//    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
+//    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
+//    	assertTrue(false);
+//    }
+//    
+//    @Test
+//    public void testeComTrica() {
+//    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
+//    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
+//    	assertTrue(false);
+//    }
+//    
+//    @Test
+//    public void testeComSequencia() {
+//    	String [] jogador1 = new String[] {"AD", "3D", "1D", "4D", "7D"};
+//    	String [] jogador2 = new String[] {"5D", "3D", "1D", "4D", "7D"};
+//    	assertTrue(false);
+//    }
 }
